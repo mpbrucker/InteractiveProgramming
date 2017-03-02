1bbb56d9047f34bcd709ea978cc961982ed0e5d4
Adam Novotny <adam.novotny@postrock.wall>
2017-03-02 11:24:06 -0500

    Change angles to rad

--- camera.py
+++ camera.py
@@ -6,10 +6,11 @@ from item import *
 
 class Camera:
     pos = [0, 0, 0]
+    # In rad
     angle = [0, 0, 0]
     fov = 0
 
-    def __init__(self, init_pos=[0, 0, 0], init_angle=[0, 0, 0], init_fov=60):
+    def __init__(self, init_pos=[0, 0, 0], init_angle=[0, 0, 0], init_fov=.5):
         self.pos = init_pos
         self.angle = init_angle
         self.fov = init_fov
@@ -17,9 +18,9 @@ class Camera:
     def __str__(self):
         return "Camera object at: {}, {}, {}. Angles: {}, {}, {}. Fov: {}".format(self.pos[0], self.pos[1], self.pos[2], self.angle[0], self.angle[1], self.angle[2], self.fov)
 
-    def move(self, x, y, speed=0.001):
+    def move(self, x, y, speed=0.0001):
         self.pos[0] += x*speed
-        self.pos[1] += y*speed
+        self.pos[2] += y*speed
 
     def rotate(self, yaw, pitch, roll, sensitivity=.001):
         self.angle[0] += yaw*sensitivity
@@ -40,7 +41,7 @@ class Renderer:
         """
 
         background = (255, 255, 255)
-        item = Item('Cylinder.stl', (50, 100, 1000), (35, 25, 0), 100)
+        item = Item('Cylinder.stl', (0, 0, 10), (35, 25, 0), 10)
 
         self.frame += 1
 
@@ -54,27 +55,30 @@ class Renderer:
 
         # print("View:")
         # print(view_matrix)
-        # print("Project:")
-        # print(project_matrix)
+        print("Project:")
+        print(project_matrix)
         # print("Transform:")
         # print(transform_matrix)
 
         # Debug: draw center point
         self.draw_point(canvas, (canvas.get_width()/2,canvas.get_height()/2, 1), (0, 200, 0), 8)
-        if np.dot((0, 0, 0, 1), view_matrix)[2] > 0.01:
-            self.draw_point(canvas, self.project_point((0,0,0), view_matrix, project_matrix, canvas), (125, 0, 0), 4)
 
-        for tri in item.world_points:
-            for point in tri:
-                point_view = np.dot(np.append(point, [1]), view_matrix)
+        test_points = [[0,0,0,1], [10, 10, 10, 10], [0, 10, 0, 1], [0, 0, 10, 1], [0, 10, 10, 1], [10, 10, 0, 1]]
 
-                if point_view[2] > 0.01:
-                    print("Pt:", point)
-                    print("Pt V:", point_view)
-                    print("Pt Tf:", self.project_point(point, view_matrix, project_matrix, canvas))
-                    input()
+        for point in test_points:
+            if np.dot(point, view_matrix)[2] > 0.01:
+                self.draw_point(canvas, self.project_point(point, view_matrix, project_matrix, canvas), (125, 0, 0), 4, point)
 
-                    self.draw_point(canvas, self.project_point(point, view_matrix, project_matrix, canvas), (125, 0, 0), 6)
+        # for tri in item.world_points:
+        #     for point in tri:
+        #         point_view = np.dot(np.append(point, [1]), view_matrix)
+        #
+        #         if point_view[2] > 0.01:
+        #             # print("Pt:", point)
+        #             # print("Pt V:", point_view)
+        #             # print("Pt Tf:", self.project_point(point, view_matrix, project_matrix, canvas))
+        #
+        #             self.draw_point(canvas, self.project_point(point, view_matrix, project_matrix, canvas), (125, 0, 0), 6)
 
         # for i in range(50):
         #     for j in range(50):
@@ -83,7 +87,9 @@ class Renderer:
         pygame.display.flip()
 
     def project_point(self, point, view_matrix, project_matrix, canvas):
-        xy = np.dot(np.append(point, [1]), view_matrix)
+        if len(point) == 3:
+            point = np.append(point, [1])
+        xy = np.dot(point, view_matrix)
         xy = np.dot(xy, project_matrix)
 
         xy = xy/xy[3]
@@ -94,10 +100,17 @@ class Renderer:
         return xy
 
 
-    def draw_point(self, canvas, point, color, size=1):
+    def draw_point(self, canvas, point, color, size=1, orig_coordinates=""):
         # print(point)
         # Basic clipping
         if point[2] > 0.01:
+            basicfont = pygame.font.SysFont(None, 12)
+            text = basicfont.render(str(point) + "\n" + str(orig_coordinates), True, (0, 0, 0), (255, 255, 255))
+            textrect = text.get_rect()
+            textrect.centerx = point[0]
+            textrect.centery = canvas.get_height() - int(point[1]) + 5
+            canvas.blit(text, textrect)
+
             for i in range(size):
                 for j in range(size):
                     canvas.set_at((int(point[0] + i), canvas.get_height() - int(point[1]) + j), color)
@@ -129,12 +142,10 @@ class Renderer:
         #                  [0, 0, q, -1],
         #                  [0, 0, qn, 0]])
 
-        pi180 = pi/180
-
         # Scale of x axis
-        a = aspect * (1 / tan((fov * .5) * pi180)) # Degrees to Rad
+        a = aspect * (1 / tan(fov * .5)) # Degrees to Rad
         # Scale of y axis
-        b = 1 / tan((fov * .5) * pi180) # Degrees to Rad
+        b = 1 / tan(fov * .5) # Degrees to Rad
 
         # Remaps z to [0,1]
         c = zfar / (zfar - znear)
@@ -165,11 +176,10 @@ class Renderer:
 
 
     def view_matrix(self, camera):
-        pi180 = pi/180
-        sinYaw = sin(camera.angle[0]*pi180)
-        cosYaw = cos(camera.angle[0]*pi180)
-        sinPitch = sin(camera.angle[1]*pi180)
-        cosPitch = cos(camera.angle[1]*pi180)
+        sinYaw = sin(camera.angle[0])
+        cosYaw = cos(camera.angle[0])
+        sinPitch = sin(camera.angle[1])
+        cosPitch = cos(camera.angle[1])
 
         # Modifies the axis vectors to point in the direction of the camera
         xaxis = (cosYaw, 0, -sinYaw)
@@ -228,11 +238,11 @@ if __name__ == "__main__":
     canvas = pygame.display.set_mode(window_size, 0, 32)
     clock = pygame.time.Clock()
 
-    camera = Camera(init_pos=[0,0,0], init_angle=[0, 0, 0], init_fov=90)
+    camera = Camera(init_pos=[0,0,0], init_angle=[0, 0, 0], init_fov=.5)
     renderer = Renderer()
     world = World()
 
-    item = Item('cube.stl', (0, 0, 0), (0, 0, 0), 1)
+    item = Item('Cylinder.stl', (0, 0, 0), (0, 0, 0), 1)
 
     while True:
         print(camera)
