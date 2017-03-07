4ca01de23fd6b5b773bb7aef748608a420941b59
Adam Novotny <adam.novotny@postrock.wall>
2017-03-06 19:22:28 -0500

    Rename point variables to coordinate system

--- camera.py
+++ camera.py
@@ -8,7 +8,6 @@ from item import Item
 sign = lambda x: math.copysign(1, x)
 
 class Camera:
-
     def __init__(self, init_pos=[0, 0, 0], init_angle=[0, 0, 0], init_fov=.5*pi):
         self.pos = init_pos
         self.angle = init_angle
@@ -40,6 +39,13 @@ class Camera:
 class Renderer:
     """
     The renderer class. Takes the camera, world, and canvas and draws the scene.
+
+    Note: point variables are named after the coordinate system they are defined in.
+    point_w:    world
+    point_c:    camera
+    point_p:    projection
+    point_can:  canvas
+
     """
 
     def __init__(self, camera, window_size=(1000,1000)):
@@ -50,7 +56,6 @@ class Renderer:
         Draws the frame and updates the display.
         """
 
-        # print(camera.angle)
         # Reset to white
         background = (255, 255, 255)
         canvas.fill(background)
@@ -59,24 +64,22 @@ class Renderer:
 
         self.draw_ground(canvas, camera)
 
-        # Draw center point
 
         for item in world.get_objects():
             for tri in item.world_points:
-                # print("Tri:", tri)
-                # self.project_line(canvas, tri[0], tri[1], view_matrix, self.project_matrix, (125, 0, 0))
+                self.project_line(canvas, tri[0], tri[1], view_matrix, self.project_matrix, (125, 0, 0))
+                self.project_line(canvas, tri[1], tri[2], view_matrix, self.project_matrix, (125, 0, 0))
+                self.project_line(canvas, tri[2], tri[0], view_matrix, self.project_matrix, (125, 0, 0))
 
-                for point in tri:
-                    transformed_point = self.project_point(point, view_matrix, self.project_matrix, canvas)
-                    self.draw_point(canvas, transformed_point, (125, 0, 0), 6)
-
-        # test_lines = (((0,1,0,1),(1,0,0,1)),) #, ((0,0,1,1),(10,10,10,1)))
-        # for line in test_lines:
-        #     self.project_line(canvas, line[0], line[1], view_matrix, self.project_matrix, (125, 0, 0), 3)
+        # Draw center point
         self.draw_point(canvas, (0,0, .01), (0, 200, 0), 6)
         pygame.display.flip()
 
     def draw_ground(self, canvas, camera):
+        """
+        Draws the ground. A rectangle based on camera angle
+        """
+
         fov = camera.fov
         cur_angle = (camera.angle[1]+(fov/2))/fov
         # print(camera.angle)
@@ -88,160 +91,175 @@ class Renderer:
         pygame.draw.rect(canvas, (120,120,120), (0,canvas.get_height()-height,canvas.get_width(),canvas.get_height()), 0)
 
 
-    def project_point(self, point, view_matrix, project_matrix, canvas):
+    def project_point(self, point_w, view_matrix, project_matrix, canvas):
+        """
+        Projects a point from world coordinates to the projected coordinates.
+        """
+
         # R3 points need an w value for perspective. Turn an [x, y, z] to [x, y, z, 1]
-        if len(point) == 3:
-            point = np.append(point, [1])
+        if len(point_w) == 3:
+            point_w = np.append(point_w, [1])
 
-        # Project the points to the camera view and then a projection view
-        # print(view_matrix)
-        # print(project_matrix)
-
-        # print("point:", point)
-        xy = np.dot(point, view_matrix)
-        # print("xyV:", xy)
-        xy = np.dot(xy, project_matrix)
-        # print("xyP:", xy)
-
+        point_v = np.dot(point_w, view_matrix)
+        point_p = np.dot(point_v, project_matrix)
 
         # Makes the perspective happen. w is based on z, and adjusts x and y properly
-        if not xy[3] == 0:
-            xy = xy/(xy[3])
+        if point_p[3] != 0:
+            point_p = point_p/(point_p[3])
 
-        # print("xyN:", xy)
+        return point_p
 
-        return xy
+    def clip_point(self, point_p):
+        """
+        Clips a point to the inside of the projection box.
+        """
 
-    def cull_point(self, point):
-        if -1 < point[0] < 1 and -1 < point[1] < 1 and -1 < point[2] < 1:
-            return point
+        if -1 < point_p[0] < 1 and -1 < point_p[1] < 1 and -1 < point_p[2] < 1:
+            return point_p
         else:
             return None
 
-    def cull_line(self, point0, point1):
-        point0_c = self.cull_point(point0)
-        point1_c = self.cull_point(point1)
+    def clip_line(self, point0_p, point1_p):
+        """
+        Clips a line to the inside of the projection box.
+        Note: incomplete
+        """
 
-        if point0_c != None and point1_c != None:
-            return point0, point1
+        point0_clip = self.clip_point(point0_p)
+        point1_clip = self.clip_point(point1_p)
 
-        # If vertical
-        if point0[0] == point1[0]:
-            if -1 < point0[0] < 1:
-                if not point0_c:
-                    point0[2] = sign(point0[2])
-                return point0, point1
+        # If both points are inside
+        if (point0_clip is not None) and (point1_clip is not None):
+            return point0_p, point1_p
+
+        # If line is vertical
+        if point0_p[0] == point1_p[0]:
+            if -1 < point0_p[0] < 1:
+                if point0_clip is None:
+                    point0_p[2] = sign(point0_p[2])
+                return point0_p, point1_p
 
 
-        if point0[0] > point1[0]:
-            point_hold = point0
-            point0 = point1
-            point1 = point_hold
+        # Make point 0 leftmost
+        if point0_p[0] > point1_p[0]:
+            point_hold = point0_p
+            point0_p = point1_p
+            point1_p = point_hold
 
         # Find intercept
-        slope = (point1[1] - point0[1]) / (point1[0] - point0[0])
-        offset = point0[1] - slope*point0[0]
+        slope = (point1_p[1] - point0_p[1]) / (point1_p[0] - point0_p[0])
+        offset = point0_p[1] - slope*point0_p[0]
         line = lambda x: slope*x + offset
 
         # Todo: Fix Z
-        if self.cull_point((-1, line(-1), 0),):
-            point0 = (-1, line(-1), point0[2])
+        if self.clip_point((-1, line(-1), 0),) is not None:
+            point0_p = (-1, line(-1), point0_p[2])
 
-        return point0, point1
+        return point0_p, point1_p
 
-
-    def project_line(self, canvas, point0, point1, view_matrix, project_matrix, color, size=1):
+    def project_line(self, canvas, point0_w, point1_w, view_matrix, project_matrix, color, size=1):
         """
         Projects and draws a line given world coordinates.
         """
 
         try:
-            if len(point0) == 3:
-                point0 = np.append(point0, [1])
-            if len(point1) == 3:
-                point1 = np.append(point1, [1])
+            if len(point0_w) == 3:
+                point0_w = np.append(point0_w, [1])
+            if len(point1_w) == 3:
+                point1_w = np.append(point1_w, [1])
         except:
-            print("Bad points given ({}, {})".format(point0, point1))
+            print("Bad points given ({}, {})".format(point0_w, point1_w))
             return
 
-        point0_p = self.project_point(point0, view_matrix, project_matrix, canvas)
-        point1_p = self.project_point(point1, view_matrix, project_matrix, canvas)
+        point0_p = self.project_point(point0_w, view_matrix, project_matrix, canvas)
+        point1_p = self.project_point(point1_w, view_matrix, project_matrix, canvas)
 
-        point0_cull, point1_cull = self.cull_line(point0_p, point1_p)
+        point0_clip, point1_clip = self.clip_line(point0_p, point1_p)
 
-        point0_can = self.norm_to_canvas_coord(canvas, point0_cull)
-        point1_can = self.norm_to_canvas_coord(canvas, point1_cull)
+        point0_can = self.norm_to_canvas_coord(canvas, point0_clip)
+        point1_can = self.norm_to_canvas_coord(canvas, point1_clip)
 
-        self.draw_line(canvas, point0_can, point1_can, (125, 0, 0), 3)
+        print(point0_can, point1_can)
+        self.draw_line(canvas, point0_can, point1_can, (125, 0, 0), 1)
 
-    def norm_to_canvas_coord(self, canvas, point):
-        return((point[0] * canvas.get_width()) + canvas.get_width()/2, (point[1] * canvas.get_height()) + canvas.get_height()/2, point[2])
+    def norm_to_canvas_coord(self, canvas, point_p):
+        return((point_p[0] * canvas.get_width()) + canvas.get_width()/2, (point_p[1] * canvas.get_height()) + canvas.get_height()/2, point_p[2])
 
-    def draw_point(self, canvas, point, color, size=1):
-        # print(point)
-        if (-1 <= point[0] <= 1) and (-1 <= point[1] <= 1) and (.01 <= point[2] <= 1):
-            point_canvas = self.norm_to_canvas_coord(canvas, point)
-            # print("Draw:", point_canvas, "Size:", size)
-            for i in range(size):
-                for j in range(size):
-                    canvas.set_at((int(point_canvas[0]) + i, canvas.get_height() - int(point_canvas[1]) + j), color)
+    def draw_point(self, canvas, point_p, color, size=1):
+        """
+        Draw a point given projected coordinates.
+        """
 
-    def draw_point_canvas(self, canvas, point, color, size=1):
+        if (-1 <= point_p[0] <= 1) and (-1 <= point_p[1] <= 1) and (.01 <= point_p[2] <= 1):
+            point_can = self.norm_to_canvas_coord(canvas, point_p)
+
+            if size == 1:
+                canvas.set_at((int(point_can[0]), canvas.get_height() - int(point_can[1])), color)
+
+            else:
+                # TODO center point
+                for i in range(size):
+                    for j in range(size):
+                        canvas.set_at((int(point_can[0]) + i, canvas.get_height() - int(point_can[1]) + j), color)
+
+    def draw_point_canvas(self, canvas, point_can, color, size=1):
+        """
+        Draw a point given canvas coordinates.
+        """
+
         for i in range(size):
             for j in range(size):
-                canvas.set_at((int(point[0]) + i, canvas.get_height() - int(point[1]) + j), color)
+                canvas.set_at((int(point_can[0]) + i, canvas.get_height() - int(point_can[1]) + j), color)
 
 
-    def draw_line(self, canvas, point0, point1, color, size=1):
+    def draw_line(self, canvas, point0_can, point1_can, color, size=3):
         """
         Draw line between two points.
         """
-        # print("Draw:", point0, point1)
 
-        if point0 == None or point1 == None:
-            # print("Draw_line, point is None")
+        if point0_can is None or point1_can is None:
             return
 
-        x = point0[0]
-        y = point0[1]
-        z = point0[2]
+        x = point0_can[0]
+        y = point0_can[1]
+        z = point0_can[2]
 
-        if int(point0[0]) == int(point1[0]) and int(point0[1]) == int(point1[1]):
+        if int(point0_can[0]) == int(point1_can[0]) and int(point0_can[1]) == int(point1_can[1]):
             print("Single point at {} {} {}".format(x, y, z))
-            self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, 100)
+            self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, 1)
             return
 
         # Vertical line
-        if int(point0[0]) == int(point1[0]):
-            # print("Vertical Line", x, int(point0[1]) - int(point1[1]))
-            dz = (point1[2] - point0[2]) / (point1[1] - point0[1])
+        if int(point0_can[0]) == int(point1_can[0]):
+            # print("Vertical Line", x, int(point0_can[1]) - int(point0_can[1]))
+            dz = (point1_can[2] - point0_can[2]) / (point1_can[1] - point0_can[1])
 
-            for y in range(int(point0[1]), int(point1[1])):
+            for y in range(int(point0_can[1]), int(point1_can[1])):
                 if z > .01:
                     self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, size)
                 z += dz
             return
 
         # Horizontal line
-        if int(point0[1]) == int(point1[1]):
+        if int(point0_can[1]) == int(point1_can[1]):
             # print("Horizontal Line")
-            dz = (point1[2] - point0[2]) / (point1[0] - point0[0])
+            dz = (point1_can[2] - point0_can[2]) / (point1_can[0] - point0_can[0])
 
-            for x in range(int(point0[0]), int(point1[0])):
+            for x in range(int(point0_can[0]), int(point1_can[0])):
                 if z > .01:
                     self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, size)
                 z += dz
             return
 
-        dx = (point1[0] - point0[0]) / (point1[1] - point0[1])
-        dy = (point1[1] - point0[1]) / (point1[0] - point0[0])
+        dx = (point1_can[0] - point0_can[0]) / (point1_can[1] - point0_can[1])
+        dy = (point1_can[1] - point0_can[1]) / (point1_can[0] - point0_can[0])
 
 
         if dx > dy:
             # dz depends on x
-            dz = (point1[2] - point0[2]) / (point1[0] - point0[0])
+            dz = (point1_can[2] - point0_can[2]) / (point1_can[0] - point0_can[0])
 
-            for x in range(int(point0[0]), int(point1[0])):
+            for x in range(int(point0_can[0]), int(point1_can[0])):
                 #print(x, y, z)
                 self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, size)
                 y += dy
@@ -249,9 +267,9 @@ class Renderer:
 
         else:
             # dz depends on y
-            dz = dz = (point1[2] - point0[2]) / (point1[1] - point0[1])
+            dz = dz = (point1_can[2] - point0_can[2]) / (point1_can[1] - point0_can[1])
 
-            for y in range(int(point0[1]), int(point1[1])):
+            for y in range(int(point0_can[1]), int(point1_can[1])):
                 #print(x, y, z)
                 self.draw_point_canvas(canvas, (int(x), int(y), int(z)), color, size)
                 x += dx
@@ -270,14 +288,11 @@ class Renderer:
         b = 1 / tan(fov * .5)
 
         # Remaps z to [0,1], for z-index
-            # Possible: c = -(zfar + znear) / (zfar - znear)
         c = zfar / (zfar - znear)
 
         d = 1
 
-        # Maps w to z *
-            # TODO: Figure out why this is wrong.
-            # Possible: e = -2 * (zfar * znear) / (zfar - znear)
+        # Maps w to proper z value
         e = -(znear * zfar) / (zfar - znear)
 
 
@@ -298,19 +313,12 @@ class Renderer:
         yaxis = (sinYaw * sinPitch, cosPitch, cosYaw * sinPitch)
         zaxis = (sinYaw * cosPitch, -sinPitch, cosPitch * cosYaw)
 
-        # print(xaxis)
-        # print(yaxis)
-        # print(zaxis)
-
-        # Might be a RH matrix
         arr =  np.array([[xaxis[0],                     yaxis[0],                   zaxis[0],                0],
                          [xaxis[1],                     yaxis[1],                   zaxis[1],                0],
                          [xaxis[2],                     yaxis[2],                   zaxis[2],                0],
                          [-np.dot(xaxis, camera.pos),   -np.dot(yaxis, camera.pos), -np.dot(zaxis, camera.pos), 1]])
 
         return arr
-
-
 
 
 if __name__ == "__main__":
@@ -324,6 +332,7 @@ if __name__ == "__main__":
     camera = Camera(init_pos=[0,0,-10], init_angle=[0, 0, 0])
     renderer = Renderer(camera, window_size=window_size)
     world = World([Item('Cylinder.stl', (0, .5, 0), (0, 0, 0), 1)])
+
 
     while True:
         print(camera)
